e46482b3bb76995e8d492842189f06a2e8a9cddc
Qubittly <aiyesanoladimeji@gmail.com>
2026-09-12 16:43:58 -0400

    writing server code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use tokio::net::TcpListener;
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::net::{TcpListener, TcpStream};
 use std::error::Error;
 use std::collections::{HashSet, HashMap};
 use std::sync::{Arc, Mutex};
 use chrono::Utc;
 
+const MAX_FILE_SIZE: u64 = 1024 * 1024 * 10; // 10 MB
+const MAX_MEMBERS: usize = 10;
+
+/// Represents metadata for a file in the network.
 struct FileMetadata {
     name: String,
     size: u64,
@@ -13,6 +18,14 @@ struct FileMetadata {
     timestamp: chrono::DateTime<Utc>,
 }
 
+/// Represents a peer in the network.
+struct Peer {
+    username: String,
+    ip_address: String,
+    port: u16,
+}
+
+/// Represents a group in the network.
 struct Group {
     name: String,
     owner: String,
@@ -21,10 +34,11 @@ struct Group {
     files: Vec<(String, FileMetadata)>,
 }
 
+/// Represents a user in the network.
 struct User {
     username: String,
     groups: HashSet<String>,
-    pending_group_requests: HashSet<String>,
+    pending_requests: HashSet<String>,
     shared_files: HashSet<String>,
     ip_address: String,
     port: u16,
@@ -44,9 +58,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
         tokio::spawn(async move {
             println!("Accepted connection from {}", addr);
+            handle_connection(socket, addr).await;
         });
     }
 
     drop(listener);
     Ok(())
+}
+
+async fn handle_connection<S>(socket: S, addr: SocketAddr) 
+where
+    S: AsyncRead + AsyncWrite + Unpin + Send + 'static,
+{
+    println!("Handling connection from {}", addr);
 }
