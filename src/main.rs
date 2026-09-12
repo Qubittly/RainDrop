463694a43bdf8b2d6b29a465b718af7cd18f7b6e
Qubittly <aiyesanoladimeji@gmail.com>
2026-09-12 17:40:29 -0400

    Added new libs and created the State struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio::net::{TcpListener, TcpStream};
 use std::error::Error;
+use std::net::SocketAddr;
 use std::collections::{HashSet, HashMap};
 use std::sync::{Arc, Mutex};
 use chrono::Utc;
@@ -23,6 +24,7 @@ struct Peer {
     username: String,
     ip_address: String,
     port: u16,
+    online: bool,
 }
 
 /// Represents a group in the network.
@@ -42,6 +44,12 @@ struct User {
     shared_files: HashSet<String>,
     ip_address: String,
     port: u16,
+    online: bool,
+}
+
+struct State {
+    groups: Arc<Mutex<HashMap<String, Group>>>,
+    users: Arc<Mutex<HashMap<String, User>>>,
 }
 
 #[tokio::main]
@@ -50,9 +58,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .unwrap_or_else(|_| "127.0.0.1:8082".to_string());
     let listener = TcpListener::bind(&server_addr).await?;
 
-    let groups = Arc::new(Mutex::new(HashMap::<String, Group>::new()));
-    let users = Arc::new(Mutex::new(HashMap::<String, User>::new()));
-
+    let state = Arc::new(Mutex::new(State { groups, users }));
     loop {
         let (socket, addr) = listener.accept().await?;
 
@@ -61,9 +67,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
             handle_connection(socket, addr).await;
         });
     }
-
-    drop(listener);
-    Ok(())
 }
 
 async fn handle_connection<S>(socket: S, addr: SocketAddr) 
